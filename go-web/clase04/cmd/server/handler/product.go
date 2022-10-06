62b43ef0814815cac7b6710b2b054313984a5db8
tonyhuang07 <yuhong.huang@mercadolibre.com>
2022-10-06 11:14:41 -0300

    'Added custom response for request"

--- go-web/clase04/cmd/server/handler/product.go
+++ go-web/clase04/cmd/server/handler/product.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"github.com/tonyhuang07/web-server/go-web/clase04/internal/products"
+	"github.com/tonyhuang07/web-server/go-web/clase04/pkg/web"
 )
 
 type request struct {
@@ -28,13 +29,31 @@ func (p *Product) Store(ctx *gin.Context) {
 	var req request
 	err := ctx.ShouldBindJSON(&req)
 	if err != nil {
-		ctx.JSON(404, gin.H{"error": err.Error()})
+		ctx.JSON(404, web.NewResponse(404, nil, err.Error()))
+		return
+	}
+	if req.Name == "" {
+		ctx.JSON(400, web.NewResponse(400, nil, "name is required"))
 		return
 	}
 
+	if req.Price == 0 {
+		ctx.JSON(400, web.NewResponse(400, nil, "price is required"))
+		return
+	}
+
+	if req.Quality == 0 {
+		ctx.JSON(400, web.NewResponse(400, nil, "quality is required"))
+		return
+	}
+
+	if !req.Published {
+		ctx.JSON(400, web.NewResponse(400, nil, "published is required"))
+		return
+	}
 	product, err := p.service.Store(req.Name, req.Price, req.Quality, req.Published)
 	if err != nil {
-		ctx.JSON(404, gin.H{"error": err.Error()})
+		ctx.JSON(404, web.NewResponse(404, nil, err.Error()))
 		return
 	}
 
@@ -44,55 +63,55 @@ func (p *Product) Store(ctx *gin.Context) {
 func (p *Product) GetAll(ctx *gin.Context) {
 	token := ctx.GetHeader("token")
 	if token != os.Getenv("TOKEN") {
-		ctx.JSON(401, gin.H{"error": "invalid token"})
+		ctx.JSON(401, web.NewResponse(401, nil, "invalid token"))
 		return
 	}
 	products, err := p.service.GetAll()
 	if err != nil {
-		ctx.JSON(404, gin.H{"error": err.Error()})
+		ctx.JSON(404, web.NewResponse(404, nil, err.Error()))
 		return
 	}
 
-	ctx.JSON(200, products)
+	ctx.JSON(200, web.NewResponse(200, products, ""))
 }
 
 func (p *Product) Update(ctx *gin.Context) {
 	id, err := strconv.Atoi(ctx.Param("id"))
 	if err != nil {
-		ctx.JSON(400, gin.H{"error": "invalid id"})
+		ctx.JSON(400, web.NewResponse(400, nil, "invalid id"))
 		return
 	}
 
 	var req request
 
 	if err := ctx.ShouldBindJSON(&req); err != nil {
-		ctx.JSON(400, gin.H{"error": err.Error()})
+		ctx.JSON(400, web.NewResponse(400, nil, err.Error()))
 		return
 	}
 
 	if req.Name == "" {
-		ctx.JSON(400, gin.H{"error": "name is required"})
+		ctx.JSON(400, web.NewResponse(400, nil, "name is required"))
 		return
 	}
 
 	if req.Price == 0 {
-		ctx.JSON(400, gin.H{"error": "price is required"})
+		ctx.JSON(400, web.NewResponse(400, nil, "price is required"))
 		return
 	}
 
 	if req.Quality == 0 {
-		ctx.JSON(400, gin.H{"error": "quality is required"})
+		ctx.JSON(400, web.NewResponse(400, nil, "quality is required"))
 		return
 	}
 
 	if !req.Published {
-		ctx.JSON(400, gin.H{"error": "published is required"})
+		ctx.JSON(400, web.NewResponse(400, nil, "published is required"))
 		return
 	}
 
 	product, err := p.service.Update(id, req.Name, req.Price, req.Quality, req.Published)
 	if err != nil {
-		ctx.JSON(404, gin.H{"error": err.Error()})
+		ctx.JSON(404, web.NewResponse(404, nil, err.Error()))
 		return
 	}
 
@@ -102,37 +121,37 @@ func (p *Product) Update(ctx *gin.Context) {
 func (p *Product) Delete(ctx *gin.Context) {
 	id, err := strconv.Atoi(ctx.Param("id"))
 	if err != nil {
-		ctx.JSON(400, gin.H{"error": "invalid id"})
+		ctx.JSON(400, web.NewResponse(400, nil, "invalid id"))
 		return
 	}
 
 	if err := p.service.Delete(id); err != nil {
-		ctx.JSON(404, gin.H{"error": err.Error()})
+		ctx.JSON(404, web.NewResponse(404, nil, err.Error()))
 		return
 	}
 
-	ctx.JSON(204, gin.H{"message": "product deleted"})
+	ctx.JSON(204, web.NewResponse(204, nil, ""))
 }
 
 func (p *Product) UpdatePrice(ctx *gin.Context) {
 	id, err := strconv.Atoi(ctx.Param("id"))
 	if err != nil {
-		ctx.JSON(400, gin.H{"error": "invalid id"})
+		ctx.JSON(400, web.NewResponse(400, nil, "invalid id"))
 		return
 	}
 	var req request
 	if err := ctx.ShouldBindJSON(&req); err != nil {
-		ctx.JSON(400, gin.H{"error": err.Error()})
+		ctx.JSON(400, web.NewResponse(400, nil, err.Error()))
 		return
 	}
 	if req.Price == 0 {
-		ctx.JSON(400, gin.H{"error": "price is required"})
+		ctx.JSON(400, web.NewResponse(400, nil, "price is required"))
 		return
 	}
 	product, err := p.service.UpdatePrice(id, req.Price)
 	if err != nil {
-		ctx.JSON(404, gin.H{"error": err.Error()})
+		ctx.JSON(404, web.NewResponse(404, nil, err.Error()))
 		return
 	}
-	ctx.JSON(200, product)
+	ctx.JSON(200, web.NewResponse(200, product, ""))
 }
